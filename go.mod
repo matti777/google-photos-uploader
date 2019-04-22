713cc516d34c0eb0722f3771438e6d903f02313e
Matti Dahlbom <matti.dahlbom@qvik.fi>
2019-04-22 09:41:06 +0300

    Writing EXIF tag works

--- go.mod
+++ go.mod
@@ -4,11 +4,22 @@ go 1.12
 
 require (
 	github.com/dsoprea/go-exif v0.0.0-20190405071739-5b56a02e0a76
-	github.com/dsoprea/go-jpeg-image-structure v0.0.0-20190306182024-502ec55f4b9c
-	github.com/dsoprea/go-logging v0.0.0-20190409182557-13b4fff49234
+	github.com/dsoprea/go-jpeg-image-structure v0.0.0-20190422055009-d6f9ba25cf48
+	github.com/dsoprea/go-logging v0.0.0-20190409182557-13b4fff49234 // indirect
 	github.com/go-errors/errors v1.0.1 // indirect
 	github.com/golang/geo v0.0.0-20181008215305-476085157cff // indirect
+	github.com/golang/protobuf v1.2.0
+	github.com/google/uuid v1.1.1
+	github.com/gorilla/mux v1.7.1
+	github.com/gosuri/uilive v0.0.2 // indirect
+	github.com/gosuri/uiprogress v0.0.0-20170224063937-d0567a9d84a1
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
 	github.com/sirupsen/logrus v1.4.1
+	github.com/urfave/cli v1.20.0
 	golang.org/x/net v0.0.0-20190420063019-afa5a82059c6 // indirect
+	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a
+	golang.org/x/sync v0.0.0-20190412183630-56d357773e84 // indirect
+	google.golang.org/api v0.3.2
+	google.golang.org/appengine v1.5.0 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- internal/exif/exif.go
+++ internal/exif/exif.go
@@ -1,12 +1,37 @@
 package exif
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"time"
 
 	exif "github.com/dsoprea/go-exif"
 	jpeg "github.com/dsoprea/go-jpeg-image-structure"
 )
+
+const (
+	tagProcessingSoftware = "ProcessingSoftware"
+	tagDateTimeOriginal   = "DateTimeOriginal"
+	tagDateTime           = "DateTime"
+)
+
+func setTag(rootIB *exif.IfdBuilder, ifdPath, tagName, tagValue string) error {
+	fmt.Printf("setTag(): ifdPath: %v, tagName: %v, tagValue: %v\n",
+		ifdPath, tagName, tagValue)
+
+	ifdIb, err := exif.GetOrCreateIbFromRootIb(rootIB, ifdPath)
+	if err != nil {
+		return fmt.Errorf("failed to get or create IB: %v", err)
+	}
+
+	if err := ifdIb.SetStandardWithName(tagName, tagValue); err != nil {
+		return fmt.Errorf("failed to set DateTime tag: %v", err)
+	}
+
+	return nil
+}
 
 // SetDateIfNone sets the EXIF DateTime to the given Time unless it has
 // already been defined.
@@ -14,31 +39,70 @@ func SetDateIfNone(filepath string, t time.Time) error {
 	parser := jpeg.NewJpegMediaParser()
 	sl, err := parser.ParseFile(filepath)
 	if err != nil {
-		return fmt.Errorf("Failed to parse JPEG file: %v", err)
+		return fmt.Errorf("failed to parse JPEG file: %v", err)
 	}
-
-	fmt.Printf("sl = %v\n", sl)
 
 	rootIb, err := sl.ConstructExifBuilder()
 	if err != nil {
-		return fmt.Errorf("Failed to construct EXIF builder: %v", err)
+		log.Println("No EXIF; creating it from scratch")
+
+		//TODO what is wrong with this - no EXIF data end up in the JPEG
+		im := exif.NewIfdMappingWithStandard()
+		ti := exif.NewTagIndex()
+		if err := exif.LoadStandardTags(ti); err != nil {
+			return fmt.Errorf("failed to load standard tags: %v", err)
+		}
+
+		rootIb = exif.NewIfdBuilder(im, ti, exif.IfdPathStandard,
+			exif.EncodeDefaultByteOrder)
+		// if err := rootIb.AddStandardWithName(tagProcessingSoftware,
+		// 	"photos-uploader"); err != nil {
+		// 	return fmt.Errorf("failed to add ProcessingSoftware tag: %v", err)
+		// }
 	}
 
+	// Form our timestamp string
+	ts := exif.ExifFullTimestampString(t)
+
+	// Set tags in IFD0
 	ifdPath := "IFD0"
-	ifdIb, err := exif.GetOrCreateIbFromRootIb(rootIb, ifdPath)
-	if err != nil {
-		return fmt.Errorf("Failed to get or create IB: %v", err)
+	if err := setTag(rootIb, ifdPath, tagDateTime, ts); err != nil {
+		return fmt.Errorf("failed to set tag %v: %v", tagDateTime, err)
+	}
+	if err := setTag(rootIb, ifdPath, tagProcessingSoftware,
+		"photos-uploader"); err != nil {
+		return fmt.Errorf("failed to set tag %v: %v", tagDateTime, err)
 	}
 
-	fmt.Printf("ifdIb: %v\n", ifdIb.DumpToStrings())
+	// Set tags in IFD/Exif
+	ifdPath = "IFD/Exif"
+	if err := setTag(rootIb, ifdPath, tagDateTimeOriginal, ts); err != nil {
+		return fmt.Errorf("failed to set tag %v: %v", tagDateTime, err)
+	}
 
-	// now := time.Now().UTC()
-	// updatedTimestampPhrase := exif.ExifFullTimestampString(now)
+	// Update the exif segment.
+	if err := sl.SetExif(rootIb); err != nil {
+		return fmt.Errorf("failed to set EXIF to jpeg: %v", err)
+	}
 
-	// err = ifdIb.SetStandardWithName("DateTime", updatedTimestampPhrase)
-	// log.PanicIf(err)
+	// Write the modified file
+	b := new(bytes.Buffer)
+	if err := sl.Write(b); err != nil {
+		return fmt.Errorf("failed to create JPEG data: %v", err)
+	}
 
-	//TODO
+	fmt.Printf("Number of image bytes: %v\n", len(b.Bytes()))
+
+	//TODO overwrite the original file; check that JpegMediaParser closes
+	// the file properly
+	filepath = "/tmp/test.jpg"
+
+	// Save the file
+	if err := ioutil.WriteFile(filepath, b.Bytes(), 0644); err != nil {
+		return fmt.Errorf("failed to write JPEG file: %v", err)
+	}
+
+	fmt.Printf("Wrote %v\n", filepath)
 
 	return nil
 }
